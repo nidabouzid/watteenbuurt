e66e7ce7dc7f03fdacc274d8360e0ac8780ba452
nidabouzid <nbo@activeviam.com>
2026-03-23 09:44:55 +0100

    initial import

---
+++ src/main/java/com/nbodev/watteenbuurt/WatteenbuurtApplication.java
@@ -0,0 +1,13 @@
+package com.nbodev.watteenbuurt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatteenbuurtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WatteenbuurtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nbodev/watteenbuurt/WatteenbuurtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nbodev.watteenbuurt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatteenbuurtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
